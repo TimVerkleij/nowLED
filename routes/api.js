bd64473b4844f5262566e2d20a9ac3404fa0f571
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-04-26 18:14:20 +0200

    Add blinker endpoints

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
+const blinker = require('./blink')
 
 router.get('/api/v1/test', (req, res) => {
     res.send('hello!')
+})
+
+
+router.get('/api/v1/blink', (req, res) => {
+    blinker.blinkLED()
+})
+
+router.get('/api/v1/stopblink', (req, res) => {
+    blinker.endBlink()
 })
 
 module.exports = router
