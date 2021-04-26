62f5685235d6e4d6073ef290ecf7c9eb1784d2d6
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-04-26 17:42:32 +0200

    Add onoff package and code

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ dotenv.config();
 
 app.use(bodyParser())
 app.use(require('./routes/api'))
+app.use(require('./routes/blink'))
 
 console.log("listening on port: " + process.env.PORT)
 app.listen(process.env.PORT);
