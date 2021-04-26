b65674d8a7f4edaf496ec4a6a041b4cc7b4e3372
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-04-26 16:14:24 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const Express = require('express')
+const bodyParser = require('body-parser')
+
+const app = Express()
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(bodyParser())
+app.use(require('./routes/api'))
+
+console.log("listening on port: " + process.env.PORT)
+app.listen(process.env.PORT);
