1aaba05a2a2afec6fa7f65d9ea8899f4dbd8836e
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-04-26 20:16:05 +0200

    Add all endpoints

---
+++ routes/LED-controller.js
@@ -0,0 +1,54 @@
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var backwards = new Gpio(2, 'out'); //use GPIO pin 4, and specify that it is output
+var forwards = new Gpio(3, 'out');
+var start = new Gpio(4, 'out');
+var reset = new Gpio(17, 'out');
+//var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
+
+// function blinkLED() { //function to start blinking
+//     if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+//         LED.writeSync(1); //set pin state to 1 (turn LED on)
+//     } else {
+//         LED.writeSync(0); //set pin state to 0 (turn LED off)
+//     }
+// }
+
+// function endBlink() { //function to stop blinking
+//     //clearInterval(blinkInterval); // Stop blink intervals
+//     LED.writeSync(0); // Turn LED off
+//     //LED.unexport(); // Unexport GPIO to free resources
+// }
+
+async function startStop(){
+    start.writeSync(1);
+    await sleep(100);
+    start.writeSync(0);
+}
+
+async function next() {
+    forwards.writeSync(1);
+    await sleep(100);
+    forwards.writeSync(0);
+}
+
+async function previous() {
+    backwards.writeSync(1);
+    await sleep(100);
+    backwards.writeSync(0);
+}
+
+async function restart(){
+    reset.writeSync(1);
+    await sleep(100);
+    reset.writeSync(0);
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+//setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+
+module.exports = {previous, next, startStop, restart}
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,29 @@
 const router = require('express').Router()
-const blinker = require('./blink')
+const ledController = require('./LED-controller')
 
 router.get('/api/v1/test', (req, res) => {
     res.send('hello!')
 })
 
 
-router.get('/api/v1/blink', (req, res) => {
-    blinker.pressButton()
+router.get('/api/v1/startstop', (req, res) => {
+    ledController.startStop()
     res.send('Look at your beautiful lights!')
 })
 
-router.get('/api/v1/stopblink', (req, res) => {
-    blinker.endBlink()
+router.get('/api/v1/forwards', (req, res) => {
+    ledController.next()
+    res.send('Next Mode!')
+})
+
+router.get('/api/v1/backwards', (req, res) => {
+    ledController.previous()
+    res.send('Previous Mode!')
+})
+
+router.get('/api/v1/reset', (req, res) => {
+    ledController.restart()
+    res.send('Lights have been reset!')
 })
 
 module.exports = router
