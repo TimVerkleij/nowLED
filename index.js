932919647630e3e496ef033dd5bb311c0ae0129d
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-04-26 20:20:29 +0200

    Add cors to API

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = Express()
 
 const dotenv = require('dotenv');
 dotenv.config();
 
+app.use(cors())
 app.use(bodyParser())
 app.use(require('./routes/api'))
 
