290a1b6e2244e3082051d00d410ec5a2daeb8ec3
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-05-03 15:56:53 +0200

    Revert back to http

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-// const Express = require('express')
-// const bodyParser = require('body-parser')
-// const cors = require('cors')
+const Express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-// const app = Express()
+const app = Express()
 
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
-// app.use(cors())
-// app.use(bodyParser())
-// app.use(require('./routes/api'))
+app.use(cors())
+app.use(bodyParser())
+app.use(require('./routes/api'))
 
-// console.log("listening on port: " + process.env.PORT)
-// app.listen(process.env.PORT);
+console.log("listening on port: " + process.env.PORT)
+app.listen(process.env.PORT);
 
-
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
-var express = require('express');
-var app = express();
-
-// your express configuration here
-
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(8080);
-httpsServer.listen(8443);
